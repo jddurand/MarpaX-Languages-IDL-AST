eeff859266b00e98992efe08667292b6b711c67e
Jean-Damien Durand <jeandamiendurand@free.fr>
2014-07-22 12:21:14 +0200

    dev+

--- lib/MarpaX/Languages/IDL/AST/MooseX/_BaseTypes.pm
+++ lib/MarpaX/Languages/IDL/AST/MooseX/_BaseTypes.pm
@@ -1,44 +1,40 @@
 #!env perl
 
 package MarpaX::Languages::IDL::AST::MooseX::_BaseTypes;
-use Exporter 'import';
-use Moose;
-use Moose::Util::TypeConstraints;
-our @_all = qw/$_floatingPtType
-               $_signedShortInt
-               $_signedLongInt
-               $_signedLonglongInt
-               $_unsignedShortInt
-               $_unsignedLongInt
-               $_unsignedLonglongInt
-               $_charType
-               $_wideCharType
-               $_stringType
-               $_wideStringType
-               $_booleanType
-               $_octetType
-               $_objectType
-               $_valueBaseType
-               $_anyType/;
-our @EXPORT_OK = @_all;
-our %EXPORT_TAGS = ('all' => \@_all);              
+use MooseX::Types -declare => [
+                               qw/_floatingPtType
+                                  _signedShortInt
+                                  _signedLongInt
+                                  _signedLonglongInt
+                                  _unsignedShortInt
+                                  _unsignedLongInt
+                                  _unsignedLonglongInt
+                                  _charType
+                                  _wideCharType
+                                  _stringType
+                                  _wideStringType
+                                  _booleanType
+                                  _octetType
+                                  _objectType
+                                  _valueBaseType
+                                  _anyType/];
+use MooseX::Types::Moose qw/Int Str Bool Object Any/;
 
-our $_floatingPtType      = class_type({ class => 'Math::BigFloat' });
-our $_signedShortInt      = subtype({as => 'Int',  where => sub { ($_ >= -2**15) && ($_ <= (2**15 - 1))}});
-our $_signedLongInt       = subtype({as => 'Int',  where => sub { ($_ >= -2**31) && ($_ <= (2**31 - 1))}});
-our $_signedLonglongInt   = subtype({as => 'Int',  where => sub { ($_ >= -2**63) && ($_ <= (2**63 - 1))}});
-our $_unsignedShortInt    = subtype({as => 'Int',  where => sub { ($_ >=      0) && ($_ <= (2**16 - 1))}});
-our $_unsignedLongInt     = subtype({as => 'Int',  where => sub { ($_ >=      0) && ($_ <= (2**32 - 1))}});
-our $_unsignedLonglongInt = subtype({as => 'Int',  where => sub { ($_ >=      0) && ($_ <= (2**64 - 1))}});
-our $_charType            = subtype({as => 'Str',  where => sub { length($_) == 1 && ord(substr($_, 0, 1)) <= 255 }});
-our $_wideCharType        = subtype({as => 'Str',  where => sub { length($_) == 1 }});
-our $_stringType          = subtype({as => 'Str',  where => sub { my $str = $_;
-                                                 ! grep {ord(substr($str, $_, 1)) > 255} (0..length($_)) }});
-our $_wideStringType      = subtype({as => 'Str'});
-our $_booleanType         = subtype({as => 'Bool'});
-our $_octetType           = subtype({as => 'Int',  where => sub { $_ >= 0 && $_ <= 255 }});
-our $_objectType          = subtype({as => 'Object'});
-our $_valueBaseType       = subtype({as => 'Object'});
-our $_anyType             = subtype({as => 'Any'});
+class_type _floatingPtType,      { class => 'Math::BigFloat' };
+subtype    _signedShortInt,      as Int, where { ($_ >= -2**15) && ($_ <= (2**15 - 1))};
+subtype    _signedLongInt,       as Int, where { ($_ >= -2**31) && ($_ <= (2**31 - 1))};
+subtype    _signedLonglongInt,   as Int, where { ($_ >= -2**63) && ($_ <= (2**63 - 1))};
+subtype    _unsignedShortInt,    as Int, where { ($_ >=      0) && ($_ <= (2**16 - 1))};
+subtype    _unsignedLongInt,     as Int, where { ($_ >=      0) && ($_ <= (2**32 - 1))};
+subtype    _unsignedLonglongInt, as Int, where { ($_ >=      0) && ($_ <= (2**64 - 1))};
+subtype    _charType,            as Str, where { length($_) == 1 && ord(substr($_, 0, 1)) <= 255 };
+subtype    _wideCharType,        as Str, where { length($_) == 1 };
+subtype    _stringType,          as Str, where { my $str = $_; ! grep {ord(substr($str, $_, 1)) > 255} (0..length($_)) };
+subtype    _wideStringType,      as Str;
+subtype    _booleanType,         as Bool;
+subtype    _octetType,           as Int, where { $_ >= 0 && $_ <= 255 };
+subtype    _objectType,          as Object;
+subtype    _valueBaseType,       as Object;
+subtype    _anyType,             as Any;
 
 1;
