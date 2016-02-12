596a41ca3889fdd9d9f65500ee59ccbc4a3dc66d
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-02-12 07:28:49 +0100

    Impl/Perl5.pm: template

---
+++ lib/MarpaX/Languages/IDL/AST/Data/Scan/Impl/Perl5.pm
@@ -0,0 +1,17 @@
+use strict;
+use warnings FATAL => 'all';
+
+package MarpaX::Languages::IDL::AST::Data::Scan::Impl::Perl5;
+
+# ABSTRACT: IDL translation to Perl5
+
+# VERSION
+
+# AUTHORITY
+
+use Moo;
+
+with 'MarpaX::Languages::IDL::AST::Data::Scan::Role::Consumer';
+
+1;
+
