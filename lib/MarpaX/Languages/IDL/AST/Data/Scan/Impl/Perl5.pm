066908056485d36c6afcfca2220c92df40150817
Jean-Damien Durand <jeandamiendurand@free.fr>
2016-02-14 11:03:07 +0100

    Impl/Perl5.pm: perl5 transpiling dev+

--- lib/MarpaX/Languages/IDL/AST/Data/Scan/Impl/Perl5.pm
+++ lib/MarpaX/Languages/IDL/AST/Data/Scan/Impl/Perl5.pm
@@ -3,14 +3,50 @@ use warnings FATAL => 'all';
 
 package MarpaX::Languages::IDL::AST::Data::Scan::Impl::Perl5;
 
-# ABSTRACT: Perl5 default implementation consumer of MarpaX::Languages::IDL::AST::Role::Consumer
+# ABSTRACT: Perl5 default implementation for transpiling IDL
 
 # VERSION
 
 # AUTHORITY
 
 use Moo;
+use MarpaX::Languages::IDL::AST::Data::Scan::Impl::Perl5::_Perl5Types -all;
+use Types::Standard -all;
 
 extends 'MarpaX::Languages::IDL::AST::Data::Scan::Impl::_Default';
+
+has _packages => (is => 'rw', isa => HashRef[Perl5_packageType]);
+
+around dsstart => sub {
+  my ($orig, $self) = @_;
+
+  $self->_packages({});
+
+  return $self->$orig()
+};
+
+around dsopen => sub {
+  my ($orig, $self, $item) = @_;
+
+  return $self->$orig($item)
+};
+
+around dsread => sub {
+  my ($orig, $self, $item) = @_;
+
+  return $self->$orig($item)
+};
+
+around dsclose => sub {
+  my ($orig, $self, $item) = @_;
+
+  return $self->$orig($item)
+};
+
+around dsend => sub {
+  my ($orig, $self) = @_;
+
+  return $self->$orig()
+};
 
 1;
